870d57f2f5550ccfe4e6a5f2823340350a39d935
bisce <cookie.it.f@gmail.com>
2019-04-26 21:43:26 +0900

    webサイトから取ってこれるように

--- src/js/client.js
+++ src/js/client.js
@@ -1,16 +1,36 @@
 import { applyMiddleware, createStore } from "redux";
+import axios from "axios";
 import { createLogger } from "redux-logger";
-import thunk from "redux-thunk";
+import promise from "redux-promise-middleware";
 
-const reducer = (state = {}, action) => {
+const initialState = {
+  fetching: false,
+  fetched: false,
+  users: [],
+  error: null
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_USERS_PENDING":
+      return { ...state, fetching: true };
+    case "FETCH_USERS_REJECTED":
+      return { ...state, fetching: false, error: action.payload };
+    case "RECEIVE_USERS":
+      return {
+        ...state,
+        fetching: false,
+        fetched: true,
+        users: action.payload
+      };
+  }
   return state;
 };
 
-const middleware = applyMiddleware(thunk, createLogger());
+const middleware = applyMiddleware(promise(), createLogger());
 const store = createStore(reducer, middleware);
 
-store.dispatch(dispatch => {
-  dispatch({ type: "FOO" });
-  dispatch({ type: "BAR" });
+store.dispatch({
+  type: "FETCH_USERS",
+  payload: axios.get("http://localhost:18080")
 });
-store.dispatch({ type: "HOGE" });
