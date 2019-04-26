8f239f3fd9fae184d86a30804ce228476ab7afca
bisce <cookie.it.f@gmail.com>
2019-04-26 12:12:20 +0900

    dispatchに関数を渡して非同期処理を行えるように

--- src/js/client.js
+++ src/js/client.js
@@ -1,11 +1,16 @@
 import { applyMiddleware, createStore } from "redux";
 import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
 
 const reducer = (state = {}, action) => {
   return state;
 };
 
-const middleware = applyMiddleware(createLogger());
+const middleware = applyMiddleware(thunk, createLogger());
 const store = createStore(reducer, middleware);
 
-store.dispatch({ type: "FOO" });
+store.dispatch(dispatch => {
+  dispatch({ type: "FOO" });
+  dispatch({ type: "BAR" });
+});
+store.dispatch({ type: "HOGE" });
